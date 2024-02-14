23d8132d1db96f5ee426faca02754a6a3fb26e10
maheshjsdev <iammahesh062@gmail.com>
2024-02-14 18:00:00 +0530

    logo not display

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,27 +12,27 @@ $(document).ready(function () {
 
 // projectDetails in tableData 
 let tableData = [
-  { id: 1, proName: "Solo", build: 'HTML, CSS, Bootstrap, JS, Jquery', path: "/assets/projects/solo/index.html" },
-  { id: 2, proName: "Crud App", build: 'HTML, CSS, Bootstrap, AngularJS', path: "/assets/projects/crudApp/index.html" },
-  { id: 3, proName: "Organic", build: 'HTML, CSS, Bootstrap, JS, Jquery', path: "/assets/projects/organic/index.html", },
-  { id: 4, proName: "3lLogics", build: 'HTML, CSS, Bootstrap, JS, Jquery', path: "/assets/projects/3lLogics/index.html" },
-  { id: 5, proName: "Zenith", build: 'HTML, CSS, Bootstrap, JS, Jquery', path: "/assets/projects/zenith/index.html" },
-  { id: 6, proName: "Zenith Form", build: 'HTML, CSS, Bootstrap, AngularJS', path: "/assets/projects/zenithform/index.html" },
+  { id: 1, proName: "Solo", build: 'HTML, CSS, Bootstrap, JS, Jquery', path: "./assets/projects/solo/index.html" },
+  { id: 2, proName: "Crud App", build: 'HTML, CSS, Bootstrap, AngularJS', path: "./assets/projects/crudApp/index.html" },
+  { id: 3, proName: "Organic", build: 'HTML, CSS, Bootstrap, JS, Jquery', path: "./assets/projects/organic/index.html", },
+  { id: 4, proName: "3lLogics", build: 'HTML, CSS, Bootstrap, JS, Jquery', path: "./assets/projects/3lLogics/index.html" },
+  { id: 5, proName: "Zenith", build: 'HTML, CSS, Bootstrap, JS, Jquery', path: "./assets/projects/zenith/index.html" },
+  { id: 6, proName: "Zenith Form", build: 'HTML, CSS, Bootstrap, AngularJS', path: "./assets/projects/zenithform/index.html" },
   {
     id: 7, proName: "Currency Converter", build: 'HTML, CSS, Bootstrap, JS', path:
-      "/assets/projects/currencyConverter/index.html"
+      "./assets/projects/currencyConverter/index.html"
   },
-  { id: 8, proName: "Dropdown Select", build: 'HTML, CSS, AngularJS', path: "/assets/projects/dropdownSelect/index.html" },
+  { id: 8, proName: "Dropdown Select", build: 'HTML, CSS, AngularJS', path: "./assets/projects/dropdownSelect/index.html" },
   {
-    id: 9, proName: "Filter Table Data", build: 'HTML, CSS, Bootstrap, JS', path: "/assets/projects/filterTableData/index.html"
+    id: 9, proName: "Filter Table Data", build: 'HTML, CSS, Bootstrap, JS', path: "./assets/projects/filterTableData/index.html"
   },
-  { id: 10, proName: "Full Page Scroll", build: 'HTML, CSS, Jquery', path: "/assets/projects/fullPageScroll/index.html" },
-  { id: 11, proName: "Calculator", build: 'HTML, CSS, JS', path: "/assets/projects/calculator/index.html" },
-  { id: 12, proName: "Adding Box", build: 'HTML, CSS, Bootstrap, AngularJS', path: "/assets/projects/addingBox/index.html" },
-  { id: 13, proName: "Interest Calculator", build: 'HTML, CSS, AgnularJS', path: "/assets/projects/interestCalculator/index.html" },
-  { id: 14, proName: "Create Placeholder", build: 'HTML, CSS, JS', path: "/assets/projects/createplaceholder/index.html" },
-  { id: 15, proName: "Modern Furniture", build: 'HTML, CSS, Bootstrap, JS', path: "/assets/projects/modernFurniture/index.html" },
-  { id: 16, proName: "Crud Bank App", build: 'HTML, CSS, Bootstrap, OOPs JS', path: "/assets/projects/oopsCrud/crud.html" }
+  { id: 10, proName: "Full Page Scroll", build: 'HTML, CSS, Jquery', path: "./assets/projects/fullPageScroll/index.html" },
+  { id: 11, proName: "Calculator", build: 'HTML, CSS, JS', path: "./assets/projects/calculator/index.html" },
+  { id: 12, proName: "Adding Box", build: 'HTML, CSS, Bootstrap, AngularJS', path: "./assets/projects/addingBox/index.html" },
+  { id: 13, proName: "Interest Calculator", build: 'HTML, CSS, AgnularJS', path: "./assets/projects/interestCalculator/index.html" },
+  { id: 14, proName: "Create Placeholder", build: 'HTML, CSS, JS', path: "./assets/projects/createplaceholder/index.html" },
+  { id: 15, proName: "Modern Furniture", build: 'HTML, CSS, Bootstrap, JS', path: "./assets/projects/modernFurniture/index.html" },
+  { id: 16, proName: "Crud Bank App", build: 'HTML, CSS, Bootstrap, OOPs JS', path: "./assets/projects/oopsCrud/crud.html" }
 ];
 
 // Dark theme colors
@@ -130,7 +130,6 @@ window.addEventListener("resize", function () {
 let menu = document.querySelectorAll(".menuList li");
 menu.forEach((ele) => {
   ele.addEventListener("click", () => {
-    navbar.classList.remove("mobileWidth");
   })
 })
 
